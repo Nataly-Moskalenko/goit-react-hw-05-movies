ddfe2472a79050405cd69f5a5c6fdd257ebc08b3
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-04-28 18:20:47 +0300

    fixes Movies component

--- src/pages/movies/Movies.jsx
+++ src/pages/movies/Movies.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link, useSearchParams, useLocation } from 'react-router-dom';
 import apiService from 'services/apiService';
 import css from './Movies.module.css';
@@ -9,6 +9,8 @@ export default function Movies() {
   const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') ?? '';
   const location = useLocation();
+  const inputValue = useRef(query);
+  const [input, setInput] = useState(inputValue.current ?? '');
 
   useEffect(() => {
     if (query === '') {
@@ -18,7 +20,6 @@ export default function Movies() {
       try {
         const data = await apiService('/search/movie', `&query=${query}`);
         setMovies(data.results);
-        console.log(data.results);
       } catch (error) {
         console.log(error);
       }
@@ -27,6 +28,10 @@ export default function Movies() {
     fetchMovies();
   }, [query]);
 
+  const handleChange = e => {
+    setInput(e.target.value);
+  };
+
   const handleSubmit = e => {
     e.preventDefault();
     const form = e.target;
@@ -34,13 +39,18 @@ export default function Movies() {
       return setSearchParams({});
     }
     setSearchParams({ query: form.elements.query.value });
-    // form.reset();
   };
 
   return (
     <div className={css.movies}>
       <form onSubmit={handleSubmit} className={css.form}>
-        <input type="text" name="query" className={css.input} />
+        <input
+          type="text"
+          name="query"
+          className={css.input}
+          value={input}
+          onChange={handleChange}
+        />
         <button type="submit" className={css.searchButton}>
           Search
         </button>
@@ -59,15 +69,15 @@ export default function Movies() {
       {query !== '' && movies.length === 0 && (
         <div>
           <img src={image} alt="Movie poster" width="320" />
-          <p>Oops, there are no movies with this search query.</p>
-          <p>Please input new search query.</p>
+          <p>Oops, there are no movies with "{query}".</p>
+          <p>Please enter new search query.</p>
         </div>
       )}
       {query === '' && (
         <div>
           <img src={image} alt="Movie poster" width="320" />
-          <p>Your search query is empty.</p>
-          <p>Please input your search query.</p>
+          <p>What movies will we search?</p>
+          <p>Please enter your search query.</p>
         </div>
       )}
     </div>
